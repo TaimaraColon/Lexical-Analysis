813481ffb0c9e49c3bd783d274ad9f8c0f5e3134
Taimara Colón López <142189101+TaimaraColon@users.noreply.github.com>
2025-09-07 21:49:22 -0400

    Add files via upload

---
+++ A1_template.py
@@ -0,0 +1,23 @@
+import ply.lex as lex
+
+# BEGIN LEXICAL ANALYZER DEFINITION
+
+
+
+
+# END LEXICAL ANALYZER DEFINITION
+
+
+#Testing Lexical Analysis
+
+textFile = open('Program_Test.txt', 'r')
+
+data = textFile.read()
+lexer = lex.lex
+lexer.input(data)
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print(tok)
